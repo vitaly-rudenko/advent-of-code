b2f3a023ea9ec18345dd097cad50a4001177d9dd
Vitaly Rudenko <maurice.dederick@gmail.com>
2025-12-06 01:54:14 +0200

    005 V2

--- 2025/005/main.go
+++ 2025/005/main.go
@@ -23,8 +23,8 @@ func main() {
 
 	mode := "fresh"
 
-	freshIngredientRanges := []IngredientRange{}
-	availableIngredients := []int{}
+	freshRanges := []IngredientRange{}
+	available := []int{}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -46,14 +46,14 @@ func main() {
 				log.Fatal(err)
 			}
 
-			freshIngredientRanges = append(freshIngredientRanges, ingredientRange)
+			freshRanges = append(freshRanges, ingredientRange)
 		case "available":
 			availableIngredient, err := strconv.Atoi(line)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			availableIngredients = append(availableIngredients, availableIngredient)
+			available = append(available, availableIngredient)
 		default:
 			log.Fatal("Invalid mode")
 		}
@@ -63,21 +63,73 @@ func main() {
 		log.Fatal(err)
 	}
 
-	freshAndAvailableIngredients := []int{}
-
-	for _, availableIngredient := range availableIngredients {
-		for _, freshIngredientRange := range freshIngredientRanges {
-			if availableIngredient >= freshIngredientRange.min && availableIngredient <= freshIngredientRange.max {
-				freshAndAvailableIngredients = append(freshAndAvailableIngredients, availableIngredient)
+	freshAndAvailable := []int{}
+	for _, ingredient := range available {
+		for _, ingredientRange := range freshRanges {
+			if ingredient >= ingredientRange.min && ingredient <= ingredientRange.max {
+				freshAndAvailable = append(freshAndAvailable, ingredient)
 				break
 			}
 		}
 	}
 
+	removed := map[int]bool{}
+	for {
+		// TODO: Looks like a dirty trick to make flawed algorithm below work.
+		//       For the given input, it retries once.
+		changed := false
+
+		for i := range freshRanges {
+			if _, ok := removed[i]; ok {
+				continue
+			}
+
+			for j := range i {
+				if _, ok := removed[j]; ok {
+					continue
+				}
+
+				if (freshRanges[j].min >= freshRanges[i].min &&
+					freshRanges[j].max <= freshRanges[i].max) ||
+					(freshRanges[i].min >= freshRanges[j].min &&
+						freshRanges[i].max <= freshRanges[j].max) {
+					// One includes another (absorb)
+
+					freshRanges[i].min = min(freshRanges[i].min, freshRanges[j].min)
+					freshRanges[i].max = max(freshRanges[i].max, freshRanges[j].max)
+					removed[j] = true
+					changed = true
+				} else if (freshRanges[i].min <= freshRanges[j].min &&
+					freshRanges[i].max >= freshRanges[j].min) ||
+					(freshRanges[j].min <= freshRanges[i].min &&
+						freshRanges[j].max >= freshRanges[i].min) {
+					// One overlaps another (merge)
+
+					freshRanges[i].min = min(freshRanges[i].min, freshRanges[j].min)
+					freshRanges[i].max = max(freshRanges[i].max, freshRanges[j].max)
+					removed[j] = true
+					changed = true
+				}
+			}
+		}
+
+		if !changed {
+			break
+		}
+	}
+
+	freshTotal := 0
+	for i := range freshRanges {
+		if _, ok := removed[i]; !ok {
+			freshTotal += freshRanges[i].max - freshRanges[i].min + 1
+		}
+	}
+
 	log.Printf("Found %v fresh ingredient ranges and %v available ingredients: "+
-		"out of them %v are both fresh and available",
-		len(freshIngredientRanges), len(availableIngredients),
-		len(freshAndAvailableIngredients))
+		"out of them %v are both fresh and available, total fresh ingredients: %v",
+		len(freshRanges), len(available),
+		len(freshAndAvailable),
+		freshTotal)
 }
 
 func ParseIngredientRange(line string) (IngredientRange, error) {
